63472d05b7b6116229bcd24a06ea18158a0d61dd
happymuzhik <ber_kut_91@mail.ru>
2016-01-02 15:03:56 +0300

    1

--- js/bomb.js
+++ js/bomb.js
@@ -44,17 +44,17 @@
 			}
 		];
 		var Player = function(params){
-				var player = {
-						name: params && params.name||'Player',
-						health: 100,
-						maxpower: 1000,
-						color: params && params.color||'red',
-						wins: 0,
-						money: 0,
-						weapons: []
-					};
-				return player;
-			};
+			// 	var player = {
+			// 			name: params && params.name||'Player',
+			// 			health: 100,
+			// 			maxpower: 1000,
+			// 			color: params && params.color||'red',
+			// 			wins: 0,
+			// 			money: 0,
+			// 			weapons: []
+			// 		};
+			// 	return player;
+			// };
 		//////////////////////////
 		bomb = {
 			walls: ['warp','none','accelerate','stickey','elastic'],
